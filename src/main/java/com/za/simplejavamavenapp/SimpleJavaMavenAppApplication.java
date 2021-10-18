dafa3658cc7ed555495c07406fed6f52968709f1
ALICIAL <W3lc0m32021#>
2021-10-18 15:22:31 +0200

    Test

--- src/main/java/com/za/simplejavamavenapp/SimpleJavaMavenAppApplication.java
+++ src/main/java/com/za/simplejavamavenapp/SimpleJavaMavenAppApplication.java
@@ -2,12 +2,25 @@ package com.za.simplejavamavenapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @SpringBootApplication
-public class SimpleJavaMavenAppApplication {
+public class SimpleJavaMavenAppApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SimpleJavaMavenAppApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleJavaMavenAppApplication.class, args);
+	}
+
+	@RequestMapping(value = "/")
+	public String hello() {
+		return "Hello World! =D";
 	}
 
 }
