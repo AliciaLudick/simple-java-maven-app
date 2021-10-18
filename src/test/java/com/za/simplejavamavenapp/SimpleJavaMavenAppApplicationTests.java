f6c45f93594d6e836afc353217ec44dc9c105280
ALICIAL <W3lc0m32021#>
2021-10-18 13:49:00 +0200

    Test

---
+++ src/test/java/com/za/simplejavamavenapp/SimpleJavaMavenAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.za.simplejavamavenapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleJavaMavenAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
